fe48feda8cbfde8515dbe8c1039286cb50a2cb80
mathi <matdelva@gmail.com>
2019-09-12 16:41:35 -0300

    Revert "creacion del branch "feature""
    This reverts commit be6a514b593e6aef801685230ed38760a1b3d164.
    
    otra vez vulvo a modificar para modificar y revertir

--- filtro.sh
+++ filtro.sh
@@ -3,6 +3,10 @@
 cat  /etc/passwd | grep $1
 
 #comentario prueba git
+<<<<<<< HEAD
 # creacion del branch "feature"
 #cambio random para volver a modificar
+=======
+>>>>>>> parent of be6a514... creacion del branch "feature"
 
+#modificacion, para ver si puedo coregir el error de "parent of be6..."
